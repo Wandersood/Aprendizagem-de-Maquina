94ac1d97b8dc56b54131262c5bc85fca5e86220d
Wanderson Ribeiro <mecanica.wanderson@gmail.com>
2025-05-20 10:41:18 -0300

    ALTERACAO

--- aula08.py
+++ aula08.py
@@ -13,21 +13,11 @@ print(df.info())
 print('-' * 100)
 print(df.head(10))
 print('-' * 100)
+print(df.tail(10))
+print('-' * 100)
+print(df.describe())
+print('-' * 100)
 
 
-# Dados fictícios
-bancos = ['Itaú', 'Santander', 'BB', 'BTG', 'Master', 'PagSeguro', 'XP']
-lucros = [10.51, 3.78, 8.77, 3.3, 1.068, 0.631, 1.18]
 
-# Estilo do seaborn
-sns.set(style="whitegrid")
 
-# Criar gráfico de barras
-plt.figure(figsize=(10, 6))
-sns.barplot(x=lucros, y=bancos, palette="Blues_d")
-
-plt.xlabel('Lucro Líquido (em bilhões)')
-plt.ylabel('Banco')
-plt.title('Lucro Líquido por Banco')
-plt.tight_layout()
-plt.show()
